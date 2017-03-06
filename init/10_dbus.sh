03e1971a2e263627d991070dd803d2004bd2bc7f
hernandito <archi@inch.com>
2017-03-06 17:39:59 -0500

    This should be it

--- init/10_dbus.sh
+++ init/10_dbus.sh
@@ -2,6 +2,12 @@
 
 if [ -e /var/run/dbus/pid ]; then rm -f /var/run/dbus/pid; fi
 mkdir -p /var/run/dbus
-chown messagebus:messagebus /var/run/dbus
-dbus-uuidgen --ensure
+#chown messagebus:messagebus /var/run/dbus
+#dbus-uuidgen --ensure
 sleep 1
+
+sh /build/prepare.sh
+sh /build/install.sh
+
+cd /usr/lib/node_modules/sonos-http-api
+npm start
--- install.sh
+++ install.sh
@@ -1,72 +1,78 @@
 #!/bin/bash
 
-# Install what we can with apt
-
-# Per https://github.com/docker/docker/issues/6345
-# We have to do this for --net=host
-#rm /usr/bin/chfn
-#ln -s -f /bin/true /usr/bin/chfn
-
-
-mkdir -p /app/settings
-mkdir -p /app/presets
-
-apt-get -q update
-
-apt-get install -qy \
-  build-essential \
-  git \
-  mc \
-  python-software-properties \
-  screen
-
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-apt-get install -y nodejs
-
-node -v
-npm -v
-
-
-# Fix avahi-daemon not working without dbus
-#sed -i -e "s#\#enable-dbus=yes#enable-dbus=false#g" /etc/avahi/avahi-daemon.conf
-#sed -i -e "s/^rlimit-nproc/#rlimit-nproc/g" /etc/avahi/avahi-daemon.conf
-
-npm install -g babel@5
-
-cd /var/tmp/
-git clone https://github.com/jishi/node-sonos-http-api
-cd node-sonos-http-api
-
-npm install -g --unsafe-perm
-
 if [ ! -f /usr/lib/node_modules/sonos-http-api/settings/settings.json]; then
 	echo "================================================"
-	echo " Creating settings.json file "
+	echo " Installing for First Time  "
 	echo "================================================"
+	echo " Preparing  "
+	echo "================================================"
+		sh /build/prepare.sh
+	
+		mkdir -p /app/settings
+		mkdir -p /app/presets
+
+		apt-get -q update
+
+		apt-get install -qy \
+		  build-essential \
+		  git \
+		  mc \
+		  python-software-properties \
+		  screen
+
+		curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+		apt-get install -y nodejs
+
+		npm install -g n
+		n stable
+		npm install npm@latest -g
+		node -v
+		npm -v
+
+
+		# Fix avahi-daemon not working without dbus
+		#sed -i -e "s#\#enable-dbus=yes#enable-dbus=false#g" /etc/avahi/avahi-daemon.conf
+		#sed -i -e "s/^rlimit-nproc/#rlimit-nproc/g" /etc/avahi/avahi-daemon.conf
+
+		npm install -g babel@5
+
+		cd /var/tmp/
+		git clone https://github.com/jishi/node-sonos-http-api
+		cd node-sonos-http-api
+
+		npm install -g --unsafe-perm
+
+	echo "================================================" 
+	echo " Creating settings.json file "
+	echo "================================================" 
 	
 		mkdir -p /usr/lib/node_modules/sonos-http-api/settings
 		cp /build/settings.json /usr/lib/node_modules/sonos-http-api/settings/settings.json
+		cp /build/settings.json /app/settings/settings.json
+		
+		ls -l /usr/lib/node_modules/sonos-http-api/settings/
+		
+	echo " "		
+	echo "================================================" 
+	echo " Creating presets.json file "
+	echo "================================================" 	
+		
+		mkdir -p /usr/lib/node_modules/sonos-http-api/presets
+		cp /build/presets.json /usr/lib/node_modules/sonos-http-api/presets/presets.json		
+		cp /build/presets.json /app/presets/presets.json
+		
+		ls -l /usr/lib/node_modules/sonos-http-api/presets/
 		
 	echo " Done"
 	echo "================================================" 
-fi
-
-echo " "
-
-if [ ! -f /usr/lib/node_modules/sonos-http-api/presets/presets.json]; then
-	echo "================================================"
-	echo " Creating presets.json file "
-	echo "================================================"
-	
-		mkdir -p /usr/lib/node_modules/sonos-http-api/presets
-		cp /build/presets.json /usr/lib/node_modules/sonos-http-api/presets/presets.json
-	
-	echo " Done"
+	echo " Cleaning up...."
+	echo "================================================" 	
+		sh /build/cleanup.sh	
+	echo "================================================" 
+	echo " Installation Succesfully Completed! Yay!"
 	echo "================================================" 
 fi
 
 
+cd /usr/lib/node_modules/sonos-http-api
 
-
-
-#cd /usr/lib/node_modules/sonos-http-api
